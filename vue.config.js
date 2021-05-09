fe99b743044d88a1b9c77fbbc44e42404ca1a3ea
Holo <holo@HolodeMacBook-Pro.local>
2021-05-09 15:21:28 +0800

    eslint config

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,19 @@
-const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-let resolve = (dir) => {
-  return path.join(__dirname, "./", dir);
-};
+const resolve = (dir) => path.join(__dirname, './', dir);
 
 module.exports = {
   // 基本路径
-  publicPath: "./",
+  publicPath: './',
   // 构建时打包文件存放位置
-  outputDir: "dist",
+  outputDir: 'dist',
   // 静态文件存放目录
-  assetsDir: "static",
+  assetsDir: 'static',
   // 指定生成的index.html生成的路径
-  indexPath: "index.html",
+  indexPath: 'index.html',
   // 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
   filenameHashing: true,
   // 多页面打包配置
@@ -42,46 +40,25 @@ module.exports = {
   // 设置生成的 HTML 中 <link rel="stylesheet"> 和 <script> 标签的 crossorigin 属性。
   // corssorigin: false,
 
-  // webpack相关配置
-  // 会通过 webpack-merge 合并到最终的配置中
-  configureWebpack: () => {
-    const myConfig = {};
-    if (process.env.NODE_ENV === "production") {
-      // 去掉注释
-      myConfig.plugins.push(
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            output: {
-              comments: false, // 去掉注释
-            },
-            compress: {
-              warnings: false,
-              drop_console: false,
-              drop_debugger: false,
-              pure_funcs: ["console.log"], // 移除console
-            },
-          },
-        }),
-        new HtmlWebpackPlugin({
-          filename: "index.html",
-          template: resolve("/index.html"),
-          minify: {
-            removeComments: true,
-            collapseWhitespace: true,
-            minifyCSS: true,
-          },
-          inject: true,
-        }),
-        new CleanWebpackPlugin()
-      );
+  // https://webpack.docschina.org/configuration/dev-server/
+  devServer: {
+    host: '127.0.0.1',
+    port: '8080',
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        // 默认true情况下，将不接受在 HTTPS 上运行且证书无效的后端服务器
+        secure: false,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
     }
-    return myConfig;
-  },
-  // 是一个函数，会接收一个基于 webpack-chain 的 ChainableConfig 实例。允许对内部的 webpack 配置进行更细粒度的修改。
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set("@", resolve("src"))
-      .set("assets", resolve("src/assets"));
+    // before: () => {},
+    // after: () => {},
   },
 
   // css相关配置
@@ -92,7 +69,7 @@ module.exports = {
     requireModuleExtension: false,
     // 是否将组件的css单独提取到一个css文件中
     extract: true,
-    sourceMap: false,
+    sourceMap: false
     // loaderOptions: {
     //   saaa: {
     //     prependData: "@import '~url';",
@@ -104,28 +81,46 @@ module.exports = {
     //   }
     // },
   },
-
-  // https://webpack.docschina.org/configuration/dev-server/
-  devServer: {
-    host: "127.0.0.1",
-    port: "8080",
-    https: false,
-    hotOnly: false,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        // 默认true情况下，将不接受在 HTTPS 上运行且证书无效的后端服务器
-        secure: false,
-        pathRewrite: {
-          "^/api": "",
-        },
-      },
-    },
-    // before: () => {},
-    // after: () => {},
+  // 是一个函数，会接收一个基于 webpack-chain 的 ChainableConfig 实例。允许对内部的 webpack 配置进行更细粒度的修改。
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'));
   },
 
-  // 构建时开启多进程处理 babel 编译
-  parallel: require("os").cpus().length > 1,
+  // webpack相关配置
+  // 会通过 webpack-merge 合并到最终的配置中
+  configureWebpack: () => {
+    const myConfig = {};
+    if (process.env.NODE_ENV === 'production') {
+      // 去掉注释
+      myConfig.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            output: {
+              comments: false // 去掉注释
+            },
+            compress: {
+              warnings: false,
+              drop_console: false,
+              drop_debugger: false,
+              pure_funcs: ['console.log'] // 移除console
+            }
+          }
+        }),
+        new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: resolve('/index.html'),
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            minifyCSS: true
+          },
+          inject: true
+        }),
+        new CleanWebpackPlugin()
+      );
+    }
+    return myConfig;
+  }
 };
